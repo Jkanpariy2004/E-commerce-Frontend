dfcbf53286cb242c2c923cbd5705acf32f386b36
Jkanpariy2004 <jainishkanpariya@gmail.com>
2024-12-20 21:47:54 +0530

    first commit

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -1,30 +1,45 @@
-import React, { Component } from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export class Header extends Component {
     render() {
         return (
             <header>
-                <div className='container my-3'>
+                <div className="container my-3">
                     <Navbar expand="lg">
-                        <Navbar.Brand href="/" className='logo'><span>E</span> Commerce</Navbar.Brand>
+                        <Navbar.Brand href="/" className="logo">
+                            <span>E</span> Commerce
+                        </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ms-auto">
-                                <Link className='nav-link' to="/">Home</Link>
-                                <Link className='nav-link' to="/about">About Us</Link>
-                                <Link className='nav-link' to="/services">Services</Link>
-                                <Link className='nav-link' to="/projects">Projects</Link>
-                                <Link className='nav-link' to="/blogs">Blogs</Link>
-                                <Link className='nav-link' to="/contact-us">Contact Us</Link>
+                                <Link className={`${location.pathname === "/" ? "Headeractive" : ""} nav-link`} to="/">
+                                    Home
+                                </Link>
+                                <Link className={`${location.pathname === "/about" ? "Headeractive" : ""} nav-link`} to="/about">
+                                    About Us
+                                </Link>
+
+                                <Link className={`${location.pathname === "/services" ? "Headeractive" : ""} nav-link`} to="/services">
+                                    Services
+                                </Link>
+                                <Link className={`${location.pathname === "/projects" ? "Headeractive" : ""} nav-link`} to="/projects">
+                                    Projects
+                                </Link>
+                                <Link className={`${location.pathname === "/blogs" ? "Headeractive" : ""} nav-link`} to="/blogs">
+                                    Blogs
+                                </Link>
+                                <Link className={`${location.pathname === "/contact-us" ? "Headeractive" : ""} nav-link`} to="/contact-us">
+                                    Contact Us
+                                </Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
                 </div>
             </header>
-        )
+        );
     }
 }
 
-export default Header
+export default Header;
